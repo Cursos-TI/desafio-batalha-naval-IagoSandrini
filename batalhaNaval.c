f66f8248ca1dfccb6d9297f9dbdb682da15d3a06
Iago Sandrini <iagosandrini@gmail.com>
2025-08-26 13:08:47 +0000

    Nível Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,6 +9,9 @@ int main()
     // Matrizes inicializadas.
     int tabuleiro[TABULEIRO][TABULEIRO] = {0};
     int navio[NAVIO];
+    int cone[NAVIO][NAVIO];
+    // int cruz[][];
+    // int octaedro[][];
 
     // Coordenadas inicias de cada navio.
     int navio1Linha = 2, navio1Coluna = 2; // Navio horizontal
@@ -17,8 +20,12 @@ int main()
     int navio3Linha = 4, navio3Coluna = 0; // Navio diagonal
     int navio4Linha = 7, navio4Coluna = 4; // Navio diagonal
 
+    int coneLinha = 0, coneColuna = 3;
+    int cruzLinha = 6, cruzColuna = 2;
+    int centroLinha = 4, centroColuna = 7;
+
     // Posição navio horizontal
-    if (navio1Coluna + NAVIO <= TABULEIRO)
+    if (NAVIO <= TABULEIRO)
     {
         for (int i = 0; i < NAVIO; i++)
         {
@@ -39,7 +46,7 @@ int main()
     }
 
     // Posição navio vertical.
-    if (navio2Linha + NAVIO <= TABULEIRO)
+    if (NAVIO <= TABULEIRO)
     {
         for (int i = 0; i < NAVIO; i++)
         {
@@ -59,8 +66,8 @@ int main()
         printf("Erro: Navio 2 ultrapassa os limites do tabuleiro!\n");
     }
 
-// Posição navio diagonal decrescente    
-    if (navio3Linha + NAVIO <= TABULEIRO)
+    // Posição navio diagonal decrescente
+    if (NAVIO <= TABULEIRO)
     {
         for (int i = 0; i < NAVIO; i++)
         {
@@ -80,8 +87,8 @@ int main()
         printf("Erro: Navio 3 ultrapassa os limites do tabuleiro!\n");
     }
 
-    // Posição navio diagonal crescente    
-    if (navio4Linha + NAVIO <= TABULEIRO)
+    // Posição navio diagonal crescente
+    if (NAVIO <= TABULEIRO)
     {
         for (int i = 0; i < NAVIO; i++)
         {
@@ -99,6 +106,63 @@ int main()
     else
     {
         printf("Erro: Navio 4 ultrapassa os limites do tabuleiro!\n");
+    }
+
+    // Habilidade em cone.
+    if (NAVIO < TABULEIRO)
+    {
+        for (int i = 0; i < NAVIO; i++)
+        {
+            for (int j = -i; j <= i; j++)
+            {
+                if (tabuleiro[coneLinha + i][coneColuna + j] == 0)
+                {
+                    tabuleiro[coneLinha + i][coneColuna + j] = NAVIO;
+                }
+                else
+                {
+                    tabuleiro[coneLinha + i][coneColuna + j] = 5;
+                }
+            }
+        }
+    }
+
+    // Habilidade em cruz
+    if (NAVIO < TABULEIRO)
+    {
+        for (int i = -2; i < NAVIO; i++)
+        {
+
+            for (int j = 0; j < NAVIO; j++)
+            {
+                if (tabuleiro[cruzLinha + i][cruzColuna] == 0)
+                {
+                    tabuleiro[cruzLinha + i][cruzColuna] = NAVIO;
+                    tabuleiro[cruzLinha][cruzColuna + i] = NAVIO;
+                }
+                else
+                {
+                    tabuleiro[cruzLinha + i][cruzColuna] = 5;
+                    tabuleiro[cruzLinha][cruzColuna + i] = 5;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < NAVIO; i++)
+    {
+        for (int j = -i; j <= i; j++)
+        {
+            tabuleiro[centroLinha + i][centroColuna + j] = 5;
+        }
+    }
+
+    for (int i = NAVIO - 2; i >= 0; i--) 
+    {
+        for (int j = -i; j <= i; j++)
+        {
+            tabuleiro[centroLinha + (2 * NAVIO - 2 - i)][centroColuna + j] = 5;
+        }
     }
 
     // Imprissão das coordenadas do tabuleiro (A,2;B,4....)
